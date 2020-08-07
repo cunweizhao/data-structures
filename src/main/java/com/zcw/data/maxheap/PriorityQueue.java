ab300febf09477464ff924732457e61fa84a841a
cunweizhao <18500103508@163.com>
2020-08-07 15:30:59 +0800

    创建优先队列

---
+++ src/main/java/com/zcw/data/maxheap/PriorityQueue.java
@@ -0,0 +1,37 @@
+package com.zcw.data.maxheap;
+
+import com.zcw.data.queue.Queue;
+
+/**
+ * @ClassName : PriorityQueue
+ * @Description : 优先队列
+ * @Author : Zhaocunwei
+ * @Date: 2020-08-07 15:25
+ */
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+    private MaxHeap<E> maxHeap;
+    public PriorityQueue(){
+        maxHeap = new MaxHeap<>();
+    }
+   @Override
+    public int getSize(){
+        return maxHeap.size();
+   }
+   @Override
+    public boolean isEmpty(){
+        return maxHeap.isEmpty();
+   }
+   @Override
+    public E getFront(){
+        return maxHeap.findMax();
+   }
+   @Override
+    public void enqueue(E e){
+        maxHeap.add(e);
+   }
+   @Override
+    public E dequeue(){
+        return maxHeap.extractMax();
+   }
+
+}
